c4df96070aed825cad8b8d0e08cb830b074505ac
Your dshepherd1965 <dshepherd21@btinternet.com>
2026-02-02 10:59:41 -0500

    Initial Commit

---
+++ emotion_detection.py
@@ -0,0 +1,9 @@
+import requests  # Import the requests library to handle HTTP requests
+
+def emotion_detector(text_to_analyse):
+    """function for emotion detection"""
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    response = requests.post(url, json = myobj, headers=header, timeout=30)
+    return response.text
