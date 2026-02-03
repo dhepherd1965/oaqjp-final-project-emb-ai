fcdeddfef42cf45fc783d65214e47931564dba86
dhepherd1965 <dshepherd21@btinternet.com>
2026-02-03 15:13:26 -0500

    task7

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,9 +8,17 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header, timeout=30)
     reply=json.loads(response.text)
+    status=response.status_code
     emotions=['anger','disgust','fear','joy','sadness']
     final_reply={}
-    for temp in emotions:
-        final_reply[temp]=reply['emotionPredictions'][0]['emotion'][temp]
-    final_reply['dominant_emotion']=sorted(final_reply,key=final_reply.get, reverse=True)[0]
-    return final_reply
+    if status==200:
+        for temp in emotions:
+            final_reply[temp]=reply['emotionPredictions'][0]['emotion'][temp]
+        final_reply['dominant_emotion']=sorted(final_reply,key=final_reply.get, reverse=True)[0]
+        return final_reply
+    else:
+        if status==400:
+            for temp in emotions:
+                final_reply[temp]=''
+            final_reply['domainant_emotion']=''
+            return final_reply
--- server.py
+++ server.py
@@ -13,8 +13,11 @@ def analysis():
     """Push button and show results"""
     texttoprocess=request.args.get('textToAnalyze')
     result=emotion_detector(texttoprocess)
-    output=f"For the given statement, the system response is 'anger' : {result['anger']}, 'disgust' : {result['disgust']}, 'fear' : {result['fear']}, 'joy' : {result['joy']}, 'sadness' : {result['sadness']}. The dominant emotion is {result['dominant_emotion']}"
-    return (str(output))
+    if result['anger']=='':
+        return("Invalid Test text try again!")
+    else:
+        output=f"For the given statement, the system response is 'anger' : {result['anger']}, 'disgust' : {result['disgust']}, 'fear' : {result['fear']}, 'joy' : {result['joy']}, 'sadness' : {result['sadness']}. The dominant emotion is {result['dominant_emotion']}"
+        return (str(output))
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1",port=5000,debug=True)
