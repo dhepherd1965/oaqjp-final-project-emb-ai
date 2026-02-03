85a4f4ba735e74eee17a80561e7743359cef742e
dhepherd1965 <dshepherd21@btinternet.com>
2026-02-03 14:46:48 -0500

    task6

--- server.py
+++ server.py
@@ -4,14 +4,17 @@ from EmotionDetection.emotion_detection import *
 app = Flask("emotionDetector")
 
 @app.route('/')
-
 def display_start():
+    """show index page"""
     return render_template("index.html")
 
-@app.route('/emotionDetector/<string:textToAnalyze>',methods=['GET','POST'])
-def runanalysus(textToAnalyze):
-    texttoprocess=request.form['textToAnalyze']
+@app.route('/emotionDetector')
+def analysis():
+    """Push button and show results"""
+    texttoprocess=request.args.get('textToAnalyze')
     result=emotion_detector(texttoprocess)
+    output=f"For the given statement, the system response is 'anger' : {result['anger']}, 'disgust' : {result['disgust']}, 'fear' : {result['fear']}, 'joy' : {result['joy']}, 'sadness' : {result['sadness']}. The dominant emotion is {result['dominant_emotion']}"
+    return (str(output))
 
-
-if __name__ == "__main__":    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="127.0.0.1",port=5000,debug=True)
