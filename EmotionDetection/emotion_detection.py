f07f7a22c2f3481ffdf827b56be2f67734b7ed82
dhepherd1965 <dshepherd21@btinternet.com>
2026-02-04 12:11:56 -0500

    Task 7

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,9 +1,10 @@
-import requests
+"""Emotion Detection function"""
 import json
-
+import requests
 def emotion_detector(text_to_analyse):
     """function for emotion detection"""
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    url = ('https://sn-watson-emotion.labs.skills.network'
+    '/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict')
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header, timeout=30)
@@ -16,9 +17,8 @@ def emotion_detector(text_to_analyse):
             final_reply[temp]=reply['emotionPredictions'][0]['emotion'][temp]
         final_reply['dominant_emotion']=sorted(final_reply,key=final_reply.get, reverse=True)[0]
         return final_reply
-    else:
-        if status==400:
-            for temp in emotions:
-                final_reply[temp]=''
-            final_reply['domainant_emotion']=''
-            return final_reply
+    if status==400:
+        for temp in emotions:
+            final_reply[temp]=''
+        final_reply['dominant_emotion']=''
+        return final_reply
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
+"""Server module for flask app"""
 from flask import Flask, render_template, request
-from EmotionDetection.emotion_detection import *
+from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("emotionDetector")
 
@@ -10,14 +11,19 @@ def display_start():
 
 @app.route('/emotionDetector')
 def analysis():
-    """Push button and show results"""
+    """Flask route for emotion detection"""
     texttoprocess=request.args.get('textToAnalyze')
     result=emotion_detector(texttoprocess)
     if result['anger']=='':
-        return("Invalid Test text try again!")
-    else:
-        output=f"For the given statement, the system response is 'anger' : {result['anger']}, 'disgust' : {result['disgust']}, 'fear' : {result['fear']}, 'joy' : {result['joy']}, 'sadness' : {result['sadness']}. The dominant emotion is {result['dominant_emotion']}"
-        return (str(output))
+        return "Invalid Test text try again!"
+
+    output=(
+    f"For the given statement, the system response is 'anger' :{result['anger']}, "
+    f"'disgust' : {result['disgust']}, 'fear' : {result['fear']},"
+    f"'joy' : {result['joy']},"
+    f"'sadness' : {result['sadness']}. The dominant emotion is {result['dominant_emotion']}"
+    )
+    return str(output)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1",port=5000,debug=True)
